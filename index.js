9a7fc9a50edb490e15a93312e870cdd3ed85e9a1
Federico Monteverdi <monteverdi.102@gmail.com>
2023-04-28 14:51:24 -0500

    updates

---
+++ index.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('.main');
+
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+let operand1;
+let operand2;
+let operator;
+
+function operate(operand1, operand2, operator){
+    return add;
+}

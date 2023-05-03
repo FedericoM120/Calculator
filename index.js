036b815611b1a9a109cbc1388e2c6d121e6fb55f
Federico Monteverdi <monteverdi.102@gmail.com>
2023-05-03 15:27:41 -0500

    updates

--- index.js
+++ index.js
@@ -36,6 +36,89 @@ function operate(){
 }
 
 //const displayScreen = document.querySelector("displayed")[0];
+function display1() {
+    let display = document.getElementsByClassName("displayed")[0];
+    if (operator == null) {
+        display.innerHTML += "1";
+        operand1 = parseInt(display.innerHTML);
+    } else if (operatorCount < 2){
+        display.innerHTML += "1";
+        operand2 = parseInt(display.innerHTML);
+    } else {
+        display.innerHTML = "1";
+        operand2 = parseInt(display.innerHTML);
+    }
+}
+
+function display2() {
+    let display = document.getElementsByClassName("displayed")[0];
+    if (operator == null) {
+        display.innerHTML += "2";
+        operand1 = parseInt(display.innerHTML);
+    } else if (operatorCount < 2){
+        display.innerHTML += "2";
+        operand2 = parseInt(display.innerHTML);
+    } else {
+        display.innerHTML = "2";
+        operand2 = parseInt(display.innerHTML);
+    }
+}
+
+function display3() {
+    let display = document.getElementsByClassName("displayed")[0];
+    if (operator == null) {
+        display.innerHTML += "3";
+        operand1 = parseInt(display.innerHTML);
+    } else if (operatorCount < 2){
+        display.innerHTML += "3";
+        operand2 = parseInt(display.innerHTML);
+    } else {
+        display.innerHTML = "3";
+        operand2 = parseInt(display.innerHTML);
+    }
+}
+
+function display4() {
+    let display = document.getElementsByClassName("displayed")[0];
+    if (operator == null) {
+        display.innerHTML += "4";
+        operand1 = parseInt(display.innerHTML);
+    } else if (operatorCount < 2){
+        display.innerHTML += "4";
+        operand2 = parseInt(display.innerHTML);
+    } else {
+        display.innerHTML = "4";
+        operand2 = parseInt(display.innerHTML);
+    }
+}
+
+function display5() {
+    let display = document.getElementsByClassName("displayed")[0];
+    if (operator == null) {
+        display.innerHTML += "5";
+        operand1 = parseInt(display.innerHTML);
+    } else if (operatorCount < 2){
+        display.innerHTML += "5";
+        operand2 = parseInt(display.innerHTML);
+    } else {
+        display.innerHTML = "5";
+        operand2 = parseInt(display.innerHTML);
+    }
+}
+
+function display6() {
+    let display = document.getElementsByClassName("displayed")[0];
+    if (operator == null) {
+        display.innerHTML += "6";
+        operand1 = parseInt(display.innerHTML);
+    } else if (operatorCount < 2){
+        display.innerHTML += "6";
+        operand2 = parseInt(display.innerHTML);
+    } else {
+        display.innerHTML = "6";
+        operand2 = parseInt(display.innerHTML);
+    }
+}
 
 function display7() {
     let display = document.getElementsByClassName("displayed")[0];
@@ -53,10 +136,14 @@ function display7() {
 
 function display8() {
     let display = document.getElementsByClassName("displayed")[0];
-    display.innerHTML += "8";
     if (operator == null) {
+        display.innerHTML += "8";
         operand1 = parseInt(display.innerHTML);
+    } else if (operatorCount < 2){
+        display.innerHTML += "8";
+        operand2 = parseInt(display.innerHTML);
     } else {
+        display.innerHTML = "8";
         operand2 = parseInt(display.innerHTML);
     }
 }
@@ -76,6 +163,7 @@ function cleared() {
     operand1 = null;
     operand2 = null;
     operator = null;
+    operatorCount = 0;
 }
 
 function addSign(){
@@ -84,17 +172,48 @@ function addSign(){
         document.getElementsByClassName("displayed")[0].innerHTML = "";
         operatorCount++;
     }else if (operator != null){
-        operatorCount++;
-        result = add(operand1, operand2);
-        operand1 = result;
-        operand2 = 0;
-        document.getElementsByClassName("displayed")[0].innerHTML = result.toString();
+        if (operator == "*"){
+            operatorCount++;
+            result = multiply(operand1, operand2);
+            operand1 = result;
+            result = add(operand1, operand2);
+            document.getElementsByClassName("displayed")[0].innerHTML = result.toString();
+        } else {
+            operatorCount++;
+            result = add(operand1, operand2);
+            operand1 = result;
+            operand2 = 0;
+            document.getElementsByClassName("displayed")[0].innerHTML = result.toString();
+        }
     }
 }
 
 function minusSign(){
-    operator = "-";
-    document.getElementsByClassName("displayed")[0].innerHTML = "";
+    if (operator == null) {
+        operator = "-";
+        document.getElementsByClassName("displayed")[0].innerHTML = "";
+        operatorCount++;
+    } else if (operator != null){
+        if (operator == "*"){
+            operatorCount++;
+            result = multiply(operand1, operand2);
+            operand1 = result;
+            result = add(operand1, operand2);
+            document.getElementsByClassName("displayed")[0].innerHTML = result.toString();
+        } else if (operator == "+") {
+            operatorCount++;
+            result = add(operand1, operand2);
+            operand1 = result;
+            operator = "-";
+            document.getElementsByClassName("displayed")[0].innerHTML = result.toString();
+        } else {
+            operatorCount++;
+            result = subtract(operand1, operand2);
+            operand1 = result;
+            operand2 = 0;
+            document.getElementsByClassName("displayed")[0].innerHTML = result.toString();
+        }
+    }
 }
 
 function divideSign(){
@@ -103,8 +222,17 @@ function divideSign(){
 }
 
 function multiplySign(){
-    operator = "*";
-    document.getElementsByClassName("displayed")[0].innerHTML = "";
+    if (operator == null) {
+        operator = "*";
+        document.getElementsByClassName("displayed")[0].innerHTML = "";
+        operatorCount++;
+    }else if (operator != null){
+        operatorCount++;
+        result = multiply(operand1, operand2);
+        operand1 = result;
+        operand2 = 0;
+        document.getElementsByClassName("displayed")[0].innerHTML = result.toString();
+    }
 }
 
 
